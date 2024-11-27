ced78424698009e7ab0ec3017c02a17cc3ba8cf3
bubu lamb <71304697+bu-bu-xxx@users.noreply.github.com>
2024-11-28 03:10:55 +0800

    add 1

---
+++ 123/2931541292/modinfo.lua
@@ -0,0 +1,18 @@
+name = "Trashcan"
+description = "Modified from YkLovin's trashcan and smith3's Additional Item Package. In case of infringement, I will apologize and delete this module. 原作者修改自YkLovin的垃圾桶和smith3的附加物品包，我这里把垃圾桶需要二本解锁的限制解除，侵权致歉并下架此模组。"
+author = "原作者：纸鸢 & 纸鸢の老公"
+version = "1.0.5"
+forumthread = ""
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+priority = -99
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
+
+api_version = 10
+
+configuration_options =
+{
+
+}
